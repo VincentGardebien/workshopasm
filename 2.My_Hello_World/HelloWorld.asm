5a7f02928f4aa219bea1e31cad6484059b9d72c0
vinz <vincent.gardebien@epitech.eu>
2024-01-31 10:17:19 +0400

    ex 1 2

--- 2.My_Hello_World/HelloWorld.asm
+++ 2.My_Hello_World/HelloWorld.asm
@@ -9,3 +9,11 @@ section .text
     global _start
 
 _start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, hello
+    mov rdx, len_hello
+    syscall
+
+    mov rax, 60
+    syscall
--- 3.My_Strlen/Strlen.asm
+++ 3.My_Strlen/Strlen.asm
@@ -4,4 +4,13 @@ section .text
     global my_Strlen
 
 my_Strlen:
-        
+    xor rcx, rcx
+loop_strlen:
+    cmp byte[rdi + rcx], 0
+    je end_stren
+    inc rcx
+    jmp loop_strlen
+
+end_stren:
+    mov rax, rcx
+    ret
